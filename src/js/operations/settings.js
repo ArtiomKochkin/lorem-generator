b36005a7458eb5ac519326ccb5364bafdef23935
ArtiomKochkin <artiom.kochkin@yandex.ru>
2023-08-20 01:00:58 +0700

    add a function change settings

--- src/js/operations/settings.js
+++ src/js/operations/settings.js
@@ -1,3 +1,17 @@
 export function changeSettings() {
-    
+    const settings = document.querySelector(".settings");
+
+    settings.onclick = function(event) {
+        let target = event.target;
+        if (target.className == "settings__item") {
+            let settingsBlock = target.parentNode;
+            let items = settingsBlock.querySelectorAll(".settings__item");
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].classList.contains("active") && target != items[i]) {
+                    items[i].classList.remove("active");
+                }
+            }
+            target.classList.add("active");
+        }
+    }
 }
