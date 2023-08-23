e39ad27d95cdcf51ea18cecd17ad9e3c5a5cb130
Artiom Kochkin <artiom.kochkin@yandex.ru>
2023-08-23 20:07:29 +0700

    Delete src directory

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import * as functions from "./modules/functions.js";
-import * as copy from "./operations/copy.js";
-import * as settings from "./operations/settings.js";
-import * as generation from "./operations/generation.js";
-
-functions.isWebp();
-copy.copyText();
-settings.changeSettings();
-generation.generateLorem();
