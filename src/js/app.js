2fba53f3e1fd248afdfe0e7838a0951b786654a6
ArtiomKochkin <artiom.kochkin@yandex.ru>
2023-08-23 17:38:35 +0700

    add a text generation function

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import * as functions from "./modules/functions.js";
 import * as copy from "./operations/copy.js";
 import * as settings from "./operations/settings.js";
+import * as generation from "./operations/generation.js";
 
 functions.isWebp();
 copy.copyText();
 settings.changeSettings();
+generation.generateLorem();
